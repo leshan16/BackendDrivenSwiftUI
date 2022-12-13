717fcd880f29ba4fd8019dc1c31462c5091f8fb7
Alexey Apestin <>
2022-12-13 14:24:58 +0300

    create project

---
+++ BackendDrivenSwiftUI/BackendDrivenSwiftUI/BackendDrivenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackendDrivenSwiftUIApp.swift
+//  BackendDrivenSwiftUI
+//
+//  Created by Апестин Алексей Дмитриевич on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BackendDrivenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackendDrivenSwiftUI/BackendDrivenSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackendDrivenSwiftUI
+//
+//  Created by Апестин Алексей Дмитриевич on 13.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
